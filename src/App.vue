fff015cf43e79607116d725b6e689278f21da5f9
AlejandroNolasco <44596749+AlejandroNolasco@users.noreply.github.com>
2021-06-14 01:20:33 -0500

    Actu Main
    se añade views/services/router/plugin

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
       dark
     >
       <div class="d-flex align-center">
+        <!--Poner Logo de Pet -->
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -14,7 +15,10 @@
           transition="scale-transition"
           width="40"
         />
-
+        <v-toolbar-items>
+        <v-btn text to="/profile">Profile</v-btn>
+      </v-toolbar-items>
+        <!--
         <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
@@ -22,19 +26,12 @@
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
-      </div>
+        />-->
+      </div> 
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+     
     </v-app-bar>
 
     <v-main>
